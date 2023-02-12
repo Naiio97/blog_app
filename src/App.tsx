48fdfbb3c2c4cf9685f5e8b8524af08da71aa70a
Nicolas Bureš <87221883+Naiio97@users.noreply.github.com>
2023-02-12 20:48:41 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+import Login from './pages/Login'
+import MyArticle from './pages/MyArticle'
+import NewArticle from './pages/NewArticle'
+import RecentArticles from './pages/RecentArticles'
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<RecentArticles />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/myArticle" element={<MyArticle />} />
+        <Route path="/newArticle" element={<NewArticle />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { HiArrowNarrowRight } from "react-icons/hi"
+
+import '../style/nav_bar.scss'
+
+
+const NavBar = () => {
+    return (
+        <div className="background">
+            <div className="nav-bar">
+                <div className="left">
+                    <Link to='/'>Recent Articles</Link>
+                    <Link to=''>About</Link>
+                </div>
+                <div className="right">
+                    <Link to='/login'>Log in <HiArrowNarrowRight /></Link>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default NavBar;
