68f96184f47bea9c6e15e5087ec4a0263b7a2478
Nicolas Bureš <87221883+Naiio97@users.noreply.github.com>
2023-02-18 18:01:14 +0100

    finished style login from

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../style/login.scss'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login-container">
+      <form className="login-form">
+        <h1>Log in</h1>
+        <div className="inputs">
+          <label>Email</label><br />
+          <input type="email" placeholder="my@example.com"></input>
+          <br />
+          <label>Heslo</label><br />
+          <input type="password" placeholder="**********"></input>
+        </div>
+      </form>
+    </div>
   )
 }
 
