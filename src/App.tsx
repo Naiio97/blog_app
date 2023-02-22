20e12f9260b05ce26e8a0388e121a8e1da068ae0
Nicolas Bureš <87221883+Naiio97@users.noreply.github.com>
2023-02-22 22:02:09 +0100

    edit name doc

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 
 import NavBar from './components/NavBar'
 import Login from './pages/Login'
-import MyArticle from './pages/MyArticle'
+import MyArticles from './pages/MyArticles'
 import NewArticle from './pages/CreateArticle'
 import RecentArticles from './pages/RecentArticles'
+
+import ''
 
 function App() {
 
   return (
     <div className="App">
-      <NavBar />
       <Routes>
         <Route path="/" element={<RecentArticles />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/myArticle" element={<MyArticle />} />
+        <Route path="/myArticles" element={<MyArticles />} />
         <Route path="/newArticle" element={<NewArticle />} />
       </Routes>
     </div>
